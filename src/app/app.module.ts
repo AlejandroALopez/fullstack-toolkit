ae2e4eeff84e32e0d0145da447537167c89012ff
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-03-19 14:25:44 -0400

    navigation grayscale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ComponentsPageComponent } from './pages/components-page/components-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ButtonComponent,
+    DashboardComponent,
+    ComponentsPageComponent,
   ],
   imports: [
     BrowserModule,
