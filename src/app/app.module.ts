d04eeb3d70658bc293b8d39a7c853c3904f2a962
alejandro <lopez.alejandro@dali.dartmouth.edu>
2023-03-20 11:50:09 -0400

    removed unnecesary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ComponentsPageComponent } from './pages/components-page/components-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    ButtonComponent,
     DashboardComponent,
-    ComponentsPageComponent,
   ],
   imports: [
     BrowserModule,
